cf4eb40e1f6beccd77a1b601a78b900504c50845
Régis GRARE <regisgrare@hotmail.fr>
2022-05-17 12:34:42 +0200

    router OK

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,30 +1,7 @@
-import { Component, OnInit } from "@angular/core";
-import { POKEMONS } from "./mock-pokemon-list";
-import { Pokemon } from "./pokemon";
+import { Component } from "@angular/core";
 
 @Component({
   selector: "app-root",
   templateUrl: "app.component.html",
 })
-export class AppComponent implements OnInit {
-  pokemonList: Pokemon[] = POKEMONS;
-  pokemonSelected: Pokemon | undefined;
-
-  ngOnInit() {
-    console.table(this.pokemonList);
-  }
-
-  selectPokemon(pokemonId: string) {
-    const id = +pokemonId;
-    const pokemon: Pokemon | undefined = this.pokemonList.find(
-      (pokemon) => pokemon.id == +pokemonId
-    );
-    if (pokemon) {
-      console.log(`vous avez cliqué sur le pokémon ${pokemon.name}`);
-      this.pokemonSelected = pokemon;
-    } else {
-      console.log(`vous avez demandé un Pokemon qui n'existe pas`);
-      this.pokemonSelected = pokemon;
-    }
-  }
-}
+export class AppComponent {}
